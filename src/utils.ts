43b1609283bd9337ba4f9352e9962e68520cd095
Yannick Fricke <yannickfricke@googlemail.com>
2024-04-17 22:51:25 +0200

    refactor(utils): added the is_null_or_undefined/1 function
    The new function checks if the given value is null or undefined.
    It can also be used as a type guard.

--- src/utils.ts
+++ src/utils.ts
@@ -6,3 +6,22 @@
 export type Tagable<Tag extends string> = Readonly<{
 	tag: Tag;
 }>;
+
+/**
+ * Checks if the given value is null or undefined
+ *
+ * @export
+ * @param {unknown} value The value to check
+ * @return {boolean} Returns true when the value is either null or undefined. False otherwise.
+ */
+export function is_null_or_undefined(value: unknown): value is undefined | null {
+	if (value === undefined) {
+		return true;
+	}
+
+	if (value === null) {
+		return true;
+	}
+
+	return false;
+}
